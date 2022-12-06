d56a07eb4f4de130317bff653bd60f0c5bb5e87b
Reinaldo Nani <reinaldo@mimobr.com>
2022-12-06 13:05:53 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,17 +3,16 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // console.log(config) // see everything in here!
-      
-      // modify config values
-      // config.defaultCommandTimeout = 4000
-      // config.baseUrl = 'http://localhost:8000'
-      
-      // modify env var value
-      // config.env.ENVIRONMENT = 'staging'
-      
-      // IMPORTANT return the updated config object
-      // return config
+      var url = 'https://app.creditas.com/auto-refi/solicitacao/informacoes-pessoais?'
+      var utmExperiment = 'experiment=CRAT-033b&loggedIn=true'
+      var source = '&utm_source=ga4-test'
+      var medium = '&utm_medium=martech-ga4'
+      var campaign = '&utm_campaign=test-cypress-2022-12-06'
+
+      config.baseUrl = url+utmExperiment+source+medium+campaign
+
+      return config
+      // implement node event listeners here
     },
   },
 });
